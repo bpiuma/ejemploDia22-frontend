a3e658a9683ad99200450c3575ca07c947460c2c
Bernardo Piuma <bpiuma.uy@gmail.com>
2021-06-08 03:55:15 +0000

    Ejemplo finalizado

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,36 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect, useContext } from "react";
+import { Redirect } from "react-router-dom";
+import { Context } from "../store/appContext";
 import "../../styles/home.scss";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+	const [auth, setAuth] = useState(false);
+
+	const pedirData = () => {
+		fetch("https://3001-rose-puma-j57opcc3.ws-us08.gitpod.io/user", {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: localStorage.getItem("token")
+			}
+		})
+			.then(response => response.json())
+			.then(data => {
+				console.log(data);
+				data === "ACCESS DENIED" ? console.log("Necesitas loguearte") : setAuth(true);
+			});
+	};
+
+	return (
+		<div className="text-center mt-5">
+			{auth ? (
+				<Redirect to="/demo" />
+			) : (
+				<button className="bnt btn-success" onClick={pedirData}>
+					Pedir datos privados
+				</button>
+			)}
+		</div>
+	);
+};
